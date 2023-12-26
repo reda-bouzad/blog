016cb41c2f542fd6b59c39903753f476bb5def03
reda-bouzad <redabouzadfst@gmail.com>
2023-12-26 10:44:14 +0100

    devops

---
+++ resources/views/post.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+
+    <article>
+        <h1> <a href="/post"> My first Post </a> </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae veritatis neque dicta culpa veniam non aliquam. Amet, laboriosam atque dolor soluta fuga suscipit natus non et? Voluptatum nemo possimus deserunt!</p>
+    </article>
+
+    <a href="/">Go Back</a>
+
+</body>
+</html>
